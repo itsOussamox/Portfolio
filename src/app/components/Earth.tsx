e2339c43f27f9d40c72c7175f240129651b1bf01
itsOussamox <oussaroublard@gmail.com>
2024-03-02 18:56:10 +0100

    :gear: Handled a deploy bug still gotta handle Earth zoom/rotate

--- src/app/components/Earth.tsx
+++ src/app/components/Earth.tsx
@@ -1,24 +1,34 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
-import Globe, { GlobeMethods } from 'react-globe.gl';
+import { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { GlobeMethods } from 'react-globe.gl';
+import dynamic from 'next/dynamic';
+
+const Globe = dynamic(() => import('react-globe.gl'), { ssr: false });
 export default function Earth(){
     const globeRef = useRef<GlobeMethods>(null!);
-    const [sWidth, setScreen] = useState((window.innerWidth > 919 ? 919 : window.innerWidth));
-    useEffect(() => {
+    const [sWidth, setScreen] = useState(1024);
+
+    const handleResize = () => {
+        var width = 1048
+        if (typeof window !== 'undefined')
+        width = window.innerWidth;
+        if (width > 919)
+            width = 919;
+        setScreen(width);
+    }
+    useLayoutEffect(() => {
         const g = globeRef.current;
+        handleResize();
         if (g) {
             g.controls().autoRotate = true;
             g.controls().enableZoom = false;
             g.controls().autoRotateSpeed = 0.5;
             g.pointOfView({lat: 0, lng: 0, altitude: 1.7});
         }
-        window.addEventListener('resize', () => {
-            var width = window.innerWidth;
-            if (width > 919)
-                width = 919;
-            setScreen(width);
-        });     
-        return () => window.removeEventListener('resize', () => {});
+        
+        if (typeof window !== 'undefined')
+            window.addEventListener('resize', handleResize);     
+        return () => {window.removeEventListener('resize', handleResize)};
     }, []);
     return (
        <div className='absolute top-1/4 left-[60%] max-sm:left-[50%] max-sm:top-[5%]'>
