87f844bbe19730c4b5e229362b8ff32b705f7b69
itsOussamox <oussaroublard@gmail.com>
2024-03-04 17:03:20 +0100

    :gear: handled some responsive errors on project section

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -17,39 +17,44 @@ interface ProjectProps {
 }
 
 function Tool({tool} : {tool: string}){
+
     return (
-        <div><h1 className="font-semibold tools max-[1100px]:text-xs max-[1200px]:text-sm max-[1350px]:text-base text-lg text-[#915DFF]">{tool}</h1></div>
+        <div><h1 className="tool-size text-[#915DFF]">{tool}</h1></div>
     )
 }
 
 
 function Project(props : ProjectProps){
     useEffect(() => {
-        textFit(document.getElementsByClassName('tools'))
+        textFit(document.getElementsByClassName('description'), {maxFontSize: 20})
+        textFit(document.getElementsByClassName('ptitle'), {maxFontSize: 30, minFontSize: 20})
+        // textFit(document.getElementsByClassName('tools'), {maxFontSize: 20})
         window.addEventListener('resize', () => {
-            textFit(document.getElementsByClassName('tools'))
+            // textFit(document.getElementsByClassName('tools'), {maxFontSize: 20})
+            textFit(document.getElementsByClassName('ptitle'), {maxFontSize: 30, minFontSize: 20})
+            textFit(document.getElementsByClassName('description'), {maxFontSize: 20})
         })
         return (
             window.removeEventListener('resize', () => {})
         )
     }, [])
     return (
-        <div className="mt-[2px] min-w-[319px] overflow-hidden  relative snap-center snap-always first:pl-8 last:pr-8 shrink-0 w-[45%] bg-bannerColor h-[95%] flex flex-col rounded-2xl outline outline-1 outline-white/[0.3] ">
-            <h1 className="text-[#12A2FF] text-lg font-semibold mt-[2%] ml-[2%]">{props.pType}</h1>
-            <h1 className="text-white text-3xl font-semibold mt-[1%] ptitle ml-[3%]">{props.pTitle}</h1>
+        <div className="mt-[2px] relative min-w-[300px] min-h-[400px] snap-center snap-always first:pl-8 last:pr-8 shrink-0 w-[45%] bg-bannerColor h-[95%] flex flex-col justify-start rounded-2xl outline outline-1 outline-white/[0.3] ">
+            <h1 className="text-[#12A2FF] font-medium text-sm mt-[2%] ml-[2%]">{props.pType}</h1>
+            <h1 className="text-white text-2xl font-semibold mt-[1%] ptitle ml-[3%]">{props.pTitle}</h1>
             <div className="mt-[5%] w-[85%] h-[50%] outline outline-1 outline-white/[0.3] shadow-2xl mx-auto rounded-xl">
                 <Image src={props.pImage as string} alt="project" className="h-full w-full rounded-xl"/>
             </div>
-            <div className="mt-[5%] ml-[3%] h-[10%] w-[95%]">
-                <h1 className="text-white h-full w-full font-extralight tracking-[1%]">{props.pDescription}</h1>
+            <div className="mt-[5%] ml-[3%] h-[30%] w-[95%]">
+                <h1 className="text-white h-full font-extralight w-full description">{props.pDescription}</h1>
             </div>
-            <div className="relative flex ml-[3%]  items-center w-[100%] h-[10%] my-auto gap-[3%] ">
+            <div className="flex ml-[3%] justify-center w-[80%] mt-auto items-end gap-[3%]">
                 {props.children}
-                <div className="flex items-center justify-end w-full h-full overflow-hidden mr-[5%]">
-                    <Link href={props.pLink as string} target="_blank">
-                        <Image src={OpenTab} alt="redirect" className=" "/>
-                    </Link>
-                </div>
+            </div>
+            <div className="absolute top-[95%] right-[3%] overflow-hidden">
+                <Link href={props.pLink as string} target="_blank">
+                    <Image src={OpenTab} alt="redirect" className="size-[100%] max-sm:size-[75%]"/>
+                </Link>
             </div>
         </div>
     )
@@ -57,11 +62,11 @@ function Project(props : ProjectProps){
 
 export default function Projects(){
     return (
-        <div  className="w-full h-[800px] max-sm:h-[450px] relative">
-            <div className="ml-[2%] max-sm:ml-[6%] text-4xl sm:text-6xl md:text-6xl lg:text-7xl">My Projects</div>
+        <div  className="w-full h-[800px] max-sm:h-[480px] relative">
+            <div className="ml-[2%] max-sm:ml-[6%] sm:text-6xl md:text-6xl lg:text-7xl">My Projects</div>
             <div className="absolute bottom-0 left-0 rounded-full h-[50%] w-[30%] bg-[#12A2FF] blur-[350px] opacity-[0.3]"/>
-            <div id="Projects" className="mt-[3%] overflow-scroll overflow-y-hidden gap-6 no-scrollbar flex h-[650px] max-sm:h-[400px] w-full snap-x snap-mandatory relative "> 
-                <div className="snap-start shrink-0 w-[15%] h-[95%] "></div>
+            <div id="Projects" className="mt-[3%] overflow-scroll overflow-y-hidden gap-6 no-scrollbar flex h-[600px] max-sm:h-[420px] w-full snap-x snap-mandatory relative "> 
+                <div className="snap-start shrink-0 w-[20%] h-[95%] "></div>
                 
                 <Project pTitle="ft_transcendence"
                 pDescription="This is my last project at 42, a multiplayer pong game with a lot of features like a chat,
@@ -72,8 +77,6 @@ export default function Projects(){
                     <Tool tool="NextJS"/>
                     <Tool tool="NestJS"/>
                     <Tool tool="React"/>
-                    <Tool tool="Tailwind"/>
-                    <Tool tool="MatterJS"/>
                     <Tool tool="SocketIO"/>
                 </Project>
 
